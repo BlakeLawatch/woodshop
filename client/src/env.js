f83dcab2ecd169e2596e4a11049e0f2da471fa9e
Jacob Winfrey <jacob_winfrey@yahoo.com>
2024-01-04 15:17:57 -0700

    .

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'http://localhost:3000/' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'blakelawatchstudent.us.auth0.com'
+export const clientId = 'pkev0jkd4wXXTb4v8XCqQdYXjELdOoZT'
+export const audience = 'https://blakelawatchstudent.com/'
