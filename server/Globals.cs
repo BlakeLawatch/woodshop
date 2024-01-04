13025bb03f9931b252cd182ffea39fbb66655219
Blake Lawatch <blakelawatch@gmail.com>
2024-01-04 15:00:21 -0700

    woodshop

---
+++ server/Globals.cs
@@ -0,0 +1,16 @@
+global using System;
+global using System.Data;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Mvc;
+global using System.Threading.Tasks;
+global using Dapper;
+global using CodeWorks.Utils;
+
+// APPLICATION SPECIFIC
+global using woodshop.Repositories;
+global using woodshop.Services;
+global using woodshop.Models;
