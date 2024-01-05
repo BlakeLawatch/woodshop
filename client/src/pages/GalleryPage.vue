057a845dfb55f28c9375016103f7b28001e16493
Jacob Winfrey <jacob_winfrey@yahoo.com>
2024-01-04 23:04:48 -0700

    .

--- client/src/pages/GalleryPage.vue
+++ client/src/pages/GalleryPage.vue
@@ -6,7 +6,7 @@
           <h1>WOOD GOOD. LOOK AT MY WOOD.</h1>
         </div>
         <div class="col-md-12 d-flex flex-wrap justify-content-center">
-          <div class="col-md-3">
+          <div class="col-md-3 p-1">
             <div class="col-md-3">
               <img class="img-fluid max-height elevation-5"
                 src="https://static.demilked.com/wp-content/uploads/2022/05/mad-skills-crazy-woodworking-projects-6.jpg"
@@ -16,48 +16,48 @@
               src="https://media.discordapp.net/attachments/1018634227463487538/1192701010985111572/neonmammoth._An_Alien_using_a_bandsaw_in_a_wood_shop_hyper_real_767c8abf-bc72-4f7c-8202-1d5ecbddfdc5.png?ex=65aa0860&is=65979360&hm=80dce7a5ecfff5f4a41a0b616b2c2d8efa6f2673ca80292ff7a8555b12436ea5&=&format=webp&quality=lossless&width=921&height=921"
               alt="">
           </div>
-          <div class="col-md-3">
+          <div class="col-md-3 p-1">
             <img class="img-fluid max-height elevation-5"
               src="https://miro.medium.com/v2/resize:fit:1358/0*6bpYNSP606ce0l6_.jpg" alt="">
           </div>
-          <div class="col-md-3">
+          <div class="col-md-3 p-1">
             <img class="img-fluid max-height elevation-5"
               src="https://media.discordapp.net/attachments/1018634227463487538/1192699904510939167/neonmammoth._An_Alien_making_furniture_in_a_wood_shop_9482932f-b5f2-4769-b693-2b7c51d51a42.png?ex=65aa0758&is=65979258&hm=be6deca51bf86ac7744a134a5c2a44ced9477d549685d6581e16770c288c790b&=&format=webp&quality=lossless&width=921&height=921"
               alt="">
           </div>
-          <div class="col-md-3">
+          <div class="col-md-3 p-1">
             <img class="img-fluid max-height elevation-5"
               src="https://media.discordapp.net/attachments/1018634227463487538/1192699890925576292/neonmammoth._An_Alien_making_furniture_in_a_wood_shop_2af7a1cb-dc55-4c6d-a6cd-b3d979ce82e9.png?ex=65aa0755&is=65979255&hm=cdc56e4a1d5c717054c1b87f73e68b10087c0ab76be0245272e53ed6c4aa5737&=&format=webp&quality=lossless&width=921&height=921"
               alt="">
           </div>
-          <div class="col-md-3">
+          <div class="col-md-3 p-1">
             <img class="img-fluid max-height elevation-5"
               src="https://media.discordapp.net/attachments/1018634227463487538/1192701008359465051/neonmammoth._An_Alien_using_a_bandsaw_in_a_wood_shop_hyper_real_6d1f2b5f-5410-42a1-8f5f-19bbbde71bcc.png?ex=65aa085f&is=6597935f&hm=75aa9cdee81b5618ed6e7886d6b90096ccbab206c53006113c9f7da4790216a5&=&format=webp&quality=lossless&width=921&height=921"
               alt="">
           </div>
-          <div class="col-md-3">
+          <div class="col-md-3 p-1">
             <img class="img-fluid max-height elevation-5"
               src="https://media.discordapp.net/attachments/1018634227463487538/1192702746403553320/neonmammoth._An_Alien_working_in_a_woodshop_carving_a_wooden_sc_d2839491-c915-4a70-99a2-94fda49967c3.png?ex=65aa09fe&is=659794fe&hm=13f209e01acb7f1aed40e2780f9d118d3974799c2f200f5b6ca7f53646fe0ff6&=&format=webp&quality=lossless&width=921&height=921"
               alt="">
           </div>
-          <div class="col-md-3">
+          <div class="col-md-3 p-1">
             <img class="img-fluid max-height elevation-5"
               src="https://www.the-sun.com/wp-content/uploads/sites/6/2021/07/NINTCHDBPICT000669088767-1.jpg" alt="">
           </div>
-          <div class="col-md-3">
+          <div class="col-md-3 p-1">
             <img class="img-fluid max-height elevation-5"
               src="https://static.demilked.com/wp-content/uploads/2022/05/mad-skills-crazy-woodworking-projects-7.jpg"
               alt="">
           </div>
-          <div class="col-md-3">
+          <div class="col-md-3 p-1">
             <img class="img-fluid max-height elevation-5"
               src="https://i.pinimg.com/736x/7a/03/e0/7a03e0c52ae2f4c008c0f56936337ed1.jpg" alt="">
           </div>
-          <div class="col-md-3">
+          <div class="col-md-3 p-1">
             <img class="img-fluid max-height elevation-5"
               src="https://img.izismile.com/img/img3/20100301/wood_sculptures_20.jpg" alt="">
           </div>
-          <div class="col-md-3">
+          <div class="col-md-3 p-1">
             <img class="img-fluid max-height elevation-5"
               src="https://static.demilked.com/wp-content/uploads/2023/10/beautiful-woodworking-projects-1.jpg" alt="">
           </div>
@@ -68,10 +68,9 @@
 </template>
 
 <style scoped lang="scss">
-.img-fluid {
-  // margin-right: 25px;
-  max-height: 300px;
-  max-width: 100%;
-  object-fit: cover;
-}
+// .img-fluid {
+//   margin-right: 25px;
+//   max-height: 200px;
+//   max-width: 100%;
+// }
 </style>
