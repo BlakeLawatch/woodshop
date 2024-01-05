0f0082f9b25ce436fd4c74864d03f1a0d307e358
Jacob Winfrey <jacob_winfrey@yahoo.com>
2024-01-04 21:49:10 -0700

    .

--- client/src/pages/GalleryPage.vue
+++ client/src/pages/GalleryPage.vue
@@ -3,7 +3,7 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-md-12">
-          <h1>WOOD GOOD</h1>
+          <h1>WOOD GOOD, LOOK AT MY WOOD</h1>
         </div>
       </div>
     </div>
