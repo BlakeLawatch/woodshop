1017a2732e18ea55d27a70cdcefcd40d386aafc9
Jacob Winfrey <jacob_winfrey@yahoo.com>
2024-01-05 12:35:12 -0700

    GalleryPage update

--- client/src/pages/GalleryPage.vue
+++ client/src/pages/GalleryPage.vue
@@ -6,66 +6,48 @@
           <h1 class="text-light">WOOD GOOD. LOOK AT MY WOOD.</h1>
         </div>
         <div class="col-md-12 d-flex flex-wrap justify-content-center">
-          <div class="col-md-3 p-3">
-            <img class="img-fluid max-height elevation-5"
-              src="https://i.pinimg.com/736x/7a/03/e0/7a03e0c52ae2f4c008c0f56936337ed1.jpg" alt="">
-          </div>
-          <div class="col-md-3 p-3">
-            <img class="img-fluid max-height elevation-5"
-              src="https://static.demilked.com/wp-content/uploads/2022/05/mad-skills-crazy-woodworking-projects-1.jpg"
-              alt="">
-          </div>
-          <div class="col-md-3 p-3">
-            <img class="img-fluid max-height elevation-5"
-              src="https://static.demilked.com/wp-content/uploads/2022/05/mad-skills-crazy-woodworking-projects-3.jpg"
-              alt="">
-          </div>
-          <div class="col-md-3 p-3">
-            <img class="img-fluid max-height elevation-5"
-              src="https://miro.medium.com/v2/resize:fit:1358/0*6bpYNSP606ce0l6_.jpg" alt="">
-          </div>
-          <div class="col-md-3 p-3">
-            <img class="img-fluid max-height elevation-5"
-              src="https://static.demilked.com/wp-content/uploads/2022/05/mad-skills-crazy-woodworking-projects-5.jpg"
-              alt="">
-          </div>
-          <div class="col-md-3 p-3">
-            <img class="img-fluid max-height elevation-5"
-              src="https://img.izismile.com/img/img3/20100301/wood_sculptures_20.jpg" alt="">
-          </div>
-          <div class="col-md-3 p-3">
-            <img class="img-fluid max-height elevation-5"
-              src="https://static.demilked.com/wp-content/uploads/2022/05/mad-skills-crazy-woodworking-projects-8.jpg"
-              alt="">
-          </div>
-          <div class="col-md-3 p-3">
-            <img class="img-fluid max-height elevation-5"
-              src="https://media.discordapp.net/attachments/1018634227463487538/1192701008359465051/neonmammoth._An_Alien_using_a_bandsaw_in_a_wood_shop_hyper_real_6d1f2b5f-5410-42a1-8f5f-19bbbde71bcc.png?ex=65aa085f&is=6597935f&hm=75aa9cdee81b5618ed6e7886d6b90096ccbab206c53006113c9f7da4790216a5&=&format=webp&quality=lossless&width=921&height=921"
-              alt="">
-          </div>
-          <div class="col-md-3 p-3">
-            <img class="img-fluid max-height elevation-5"
-              src="https://media.discordapp.net/attachments/1018634227463487538/1192702746403553320/neonmammoth._An_Alien_working_in_a_woodshop_carving_a_wooden_sc_d2839491-c915-4a70-99a2-94fda49967c3.png?ex=65aa09fe&is=659794fe&hm=13f209e01acb7f1aed40e2780f9d118d3974799c2f200f5b6ca7f53646fe0ff6&=&format=webp&quality=lossless&width=921&height=921"
-              alt="">
-          </div>
-          <div class="col-md-3 p-3">
-            <img class="img-fluid max-height elevation-5"
-              src="https://www.the-sun.com/wp-content/uploads/sites/6/2021/07/NINTCHDBPICT000669088767-1.jpg" alt="">
-          </div>
-          <div class="col-md-3 p-3">
-            <img class="img-fluid max-height elevation-5"
-              src="https://static.demilked.com/wp-content/uploads/2022/05/mad-skills-crazy-woodworking-projects-7.jpg"
-              alt="">
-          </div>
-          <div class="col-md-3 p-3">
-            <img class="img-fluid max-height elevation-5"
-              src="https://static.demilked.com/wp-content/uploads/2023/10/beautiful-woodworking-projects-1.jpg" alt="">
+          <div class="col-md-3 p-3" v-for="(image, index) in images" :key="index">
+            <img class="img-fluid max-height elevation-5" :src="image.src" :alt="image.alt"
+              @click="enlargeImage(index)" />
           </div>
         </div>
       </div>
     </div>
+
+    <div v-if="enlargedImageIndex !== -1" class="enlarged" @click="enlargeImage(-1)">
+      <img :src="images[enlargedImageIndex].src" :alt="images[enlargedImageIndex].alt" />
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      enlargedImageIndex: -1,
+      images: [
+        { src: "https://i.pinimg.com/736x/7a/03/e0/7a03e0c52ae2f4c008c0f56936337ed1.jpg", alt: "Image 1" },
+        { src: "https://static.demilked.com/wp-content/uploads/2022/05/mad-skills-crazy-woodworking-projects-1.jpg", alt: "Image 2" },
+        { src: "https://static.demilked.com/wp-content/uploads/2022/05/mad-skills-crazy-woodworking-projects-3.jpg", alt: "Image 3" },
+        { src: "https://miro.medium.com/v2/resize:fit:1358/0*6bpYNSP606ce0l6_.jpg", alt: "Image 4" },
+        { src: "https://static.demilked.com/wp-content/uploads/2022/05/mad-skills-crazy-woodworking-projects-5.jpg", alt: "Image 5" },
+        { src: "https://img.izismile.com/img/img3/20100301/wood_sculptures_20.jpg", alt: "Image 6" },
+        { src: "https://static.demilked.com/wp-content/uploads/2022/05/mad-skills-crazy-woodworking-projects-8.jpg", alt: "Image 7" },
+        { src: "https://media.discordapp.net/attachments/1018634227463487538/1192701008359465051/neonmammoth._An_Alien_using_a_bandsaw_in_a_wood_shop_hyper_real_6d1f2b5f-5410-42a1-8f5f-19bbbde71bcc.png?ex=65aa085f&is=6597935f&hm=75aa9cdee81b5618ed6e7886d6b90096ccbab206c53006113c9f7da4790216a5&=&format=webp&quality=lossless&width=921&height=921", alt: "Image 8" },
+        { src: "https://media.discordapp.net/attachments/1018634227463487538/1192702746403553320/neonmammoth._An_Alien_working_in_a_woodshop_carving_a_wooden_sc_d2839491-c915-4a70-99a2-94fda49967c3.png?ex=65aa09fe&is=659794fe&hm=13f209e01acb7f1aed40e2780f9d118d3974799c2f200f5b6ca7f53646fe0ff6&=&format=webp&quality=lossless&width=921&height=921", alt: "Image 9" },
+        { src: "https://www.the-sun.com/wp-content/uploads/sites/6/2021/07/NINTCHDBPICT000669088767-1.jpg", alt: "Image 10" },
+        { src: "https://static.demilked.com/wp-content/uploads/2022/05/mad-skills-crazy-woodworking-projects-7.jpg", alt: "Image 11" },
+        { src: "https://static.demilked.com/wp-content/uploads/2023/10/beautiful-woodworking-projects-1.jpg", alt: "Image 12" },
+      ],
+    };
+  },
+  methods: {
+    enlargeImage(index) {
+      this.enlargedImageIndex = index;
+    },
+  },
+};
+</script>
 
 <style scoped lang="scss">
 .max-height {
@@ -73,15 +55,55 @@
   max-width: 50vh;
   height: 50vh;
   width: 100%;
+  margin-right: 25px;
+  filter: drop-shadow(1rem 1rem 10px rgba(255, 255, 255, 0.23));
+  transition: transform 0.3s ease-in-out;
+}
+
+.max-height:hover {
+  transform: scale(1.1);
+}
+
+.enlarged {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.2);
+  border-radius: 16px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(5px);
+  -webkit-backdrop-filter: blur(5px);
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 999;
+}
+
+.enlarged img {
+  max-width: 90%;
+  max-height: 90%;
+
+}
+
+.img-fluid {
+  border-radius: 15px;
 }
 
 .img-fluid:hover {
   margin-right: 25px;
-  filter: drop-shadow(1rem 1rem 10px rgba(255, 255, 255, 0.485));
+
+  filter: drop-shadow(1rem 1rem 10px rgba(0, 0, 0, 0.641));
 }
 
 .gallery {
   overflow-x: hidden;
-  background-color: rgb(46, 43, 39);
+  overflow-y: hidden;
+  background-color: rgb(202, 186, 164);
 }
 </style>
+
+
+
