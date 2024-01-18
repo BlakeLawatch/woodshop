b2b917f549aee70271ce839e16f02165a71aba6a
jakew33 <jacob_winfrey@yahoo.com>
2024-01-18 13:14:34 -0700

    ...

--- client/src/components/SlideshowComponent.vue
+++ client/src/components/SlideshowComponent.vue
@@ -1,21 +1,29 @@
 <template>
-  <div id="carouselExampleFade" class="carousel slide carousel-fade d-flex justify-content-center">
+  <div id="carouselExampleIndicators" class="carousel slide carousel-fade">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+        aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+        aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+        aria-label="Slide 3"></button>
+    </div>
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="src/assets/img/Project-images/couch-table-zoom-out.png" alt="...">
+        <img src="src/assets/img/Project-images/board-epoxy.png" alt="0">
       </div>
       <div class="carousel-item">
-        <img src="src/assets/img/Project-images/corn-hole-close.png" alt="...">
+        <img src="src/assets/img/Project-images/book-rack-backside.png" alt="1">
       </div>
       <div class="carousel-item">
-        <img src="src/assets/img/Project-images/Shoe-rack-naked.png" alt="...">
+        <img src="src/assets/img/Project-images/couch-table-zoom-out.png" alt="2">
       </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Previous</span>
     </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Next</span>
     </button>
